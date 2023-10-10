c632baa1c367dd6af9af2e6dd8498eafeb2e7364
cgp <2986779260@qq.com>
2023-10-10 22:14:35 +0800

    learn error

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -1,3 +1,10 @@
+/*
+ * @Descripttion: my code for learning
+ * @Author: chenggong Pan
+ * @Date: 2023-10-09 07:53:20
+ * @LastEditors: chenggong Pan
+ * @LastEditTime: 2023-10-10 19:41:36
+ */
 // errors2.rs
 //
 // Say we're writing a game where you can buy items with tokens. All items cost
@@ -19,15 +26,16 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-
+    let qty = item_quantity.parse::<i32>()?;
+    // match item_quantity.parse::<i32>() {
+    //     Err(err) =>  Err(err),
+    //     Ok(qty) => Ok(qty * cost_per_item + processing_fee),
+    // }
     Ok(qty * cost_per_item + processing_fee)
 }
 
--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -1,27 +1,34 @@
+/*
+ * @Descripttion: my code for learning
+ * @Author: chenggong Pan
+ * @Date: 2023-10-09 07:53:20
+ * @LastEditors: chenggong Pan
+ * @LastEditTime: 2023-10-10 21:02:36
+ */
 // errors3.rs
 //
-// This is a program that is trying to use a completed version of the
-// `total_cost` function from the previous exercise. It's not working though!
+// This is a program that is trying to use a completversion of the
+// `total_cost` function from the previous exercise. ed It's not working though!
 // Why not? What should we do to fix it?
 //
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
-
-fn main() {
+use std::error::Error;
+fn main() ->Result<(), Box<dyn Error>> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
-
+    
     if cost > tokens {
         println!("You can't afford that many!");
+        Ok(())
     } else {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
+        Ok(())
     }
 }
 
